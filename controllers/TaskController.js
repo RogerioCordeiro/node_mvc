336df0fa447054d2a56d8e5be60501c8d6c6f814
Rogerio Cordeiro <rocordeirosilva@gmail.com>
2024-09-17 21:40:23 -0300

    feat(tasks): add delete function for tasks

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -13,6 +13,11 @@ module.exports = class TaskController {
         await Task.create(task)
         res.redirect('/tasks')
     }
+    static async removeTask(req, res) {
+        const id = req.body.id
+        await Task.destroy({ where: { id: id } })
+        res.redirect('/tasks')
+    }
     static async showTasks(req, res) {
         const tasks = await Task.findAll({ raw: true })
         res.render('tasks/all', { tasks })
